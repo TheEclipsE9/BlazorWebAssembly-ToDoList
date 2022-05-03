c48587b9387eaccf36c8ad4b9cc931f1062cf15f
Aleksei <TheEclipsE@tut.by>
2022-05-03 15:58:05 +0300

    clean filedir edit navmenu

--- BlazorWebAssembly/Server/Controllers/ToDoController.cs
+++ BlazorWebAssembly/Server/Controllers/ToDoController.cs
@@ -11,12 +11,6 @@ namespace BlazorWebAssembly.Server.Controllers
     {
         private readonly ToDoListContext _dbContext;
 
-        private List<ToDoItem> ToDoItems = new List<ToDoItem>
-        {
-            new ToDoItem { Url = "gotothesupermarket", Title = "Go to the supermarket", Description = "Buy a bread" },
-            new ToDoItem { Url = "gototheGym", Title = "Go to the gym", Description = "from 8.00 to 9.00" }
-        };
-
         public ToDoController(ToDoListContext dbContext)
         {
             _dbContext = dbContext;
@@ -27,7 +21,7 @@ namespace BlazorWebAssembly.Server.Controllers
         [Route("{url}")]
         public ActionResult<ToDoItem> GetToDoItemByUrl(string url)
         {
-            var toDoItem = ToDoItems.FirstOrDefault(x => x.Url == url);
+            var toDoItem = _dbContext.ToDoItems.FirstOrDefault(x => x.Url == url);
             if (toDoItem == null)
             {
                 return NotFound("Item not found!");
