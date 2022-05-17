f8f258c9779bea5d5634e28a7c5c9ca1339d3e13
Aleksei <TheEclipsE@tut.by>
2022-05-17 15:45:45 +0300

    add validation with empty and minlenght 5 add a clean of input after create

--- BlazorWebAssembly/Shared/ToDoItem.cs
+++ BlazorWebAssembly/Shared/ToDoItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorWebAssembly.Shared
     public class ToDoItem
     {
         public int Id { get; set; }
+        [Required, MinLength(5, ErrorMessage = "Min title length is 5!")]
         public string Title { get; set; }
 
         public bool IsDone { get; set; } = false;
